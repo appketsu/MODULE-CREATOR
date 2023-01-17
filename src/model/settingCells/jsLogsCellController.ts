f2b5d1941d5df07446ae39d68c6922df03978386
Orion <Orion>
2023-01-17 19:35:56 +0100

    before trying monaco editor

--- src/model/settingCells/jsLogsCellController.ts
+++ src/model/settingCells/jsLogsCellController.ts
@@ -2,11 +2,16 @@
 
 
 import { UICollectionViewCell } from "../collectionView/collectionViewCell";
-import { TitleMessage } from "../interfaces";
+import { TitleMessage,RectNumber } from "../interfaces";
 import { clickCellView } from "../../view/settingsCells/clickCellView";
 import { jsLogsCellView } from "../../view/settingsCells/jsLogsCell";
 import { logsCellView } from "../../view/settingsCells/logsCellView";
 import $ from "jquery";
+import { ElemModalDirection, ElementModalPos } from "../elementModalView/elementModalView";
+import { InsertedViewData } from "../view/insertView";
+import { DropDown } from "../dropDownMenu/dropDown";
+import { DefaultDropDownCell } from "../dropDownMenu/dropDownCell";
+import { viewDefaultDropDownCell } from "../../view/defaultViews/defaultDropDownCellView";
 
 
 
@@ -25,9 +30,37 @@ export class JSLogsCellController extends UICollectionViewCell {
     viewWasInserted() {
         super.viewWasInserted();
         this.setData()
+        $(`[${this.id}]`).off().on("contextmenu",(e) => {
+            console.log("hello world")
+            this.didOpenContextMenu(e,() => {
+
+            });
+            return false;
+        });
 
 
     }
+
+    didOpenContextMenu(ev: JQuery.Event,completion : () => void) {
+        let modal = new DropDown();
+        let dropDownCell = new DefaultDropDownCell("Open on window",undefined,undefined,true,viewDefaultDropDownCell);
+
+        dropDownCell.viewWasInsertedCallback = (id) => {
+            $(`[${id}]`).addClass(["bg-secondary-dark-hover","pointer","tc-t-primary"])
+        }
+
+        modal.addCell(dropDownCell,(index,dropwDown) => {
+            dropwDown.finish();
+        })
+
+        modal.insertInto(new InsertedViewData(undefined,"body"))
+        modal.generalSetUp(0,10,
+            ElementModalPos.right,
+            ElemModalDirection.bottom,
+            {x:ev.clientX ?? 0,y:ev.clientY ?? 0,width:0,height:0},
+            200)
+    }
+
     async setData() {
 
         $(`[${this.id}]`).addClass([this.returnClass("error",this.error),
@@ -46,6 +79,7 @@ export class JSLogsCellController extends UICollectionViewCell {
 
 
     finish() {
+        $(`[${this.id}]`).off();
         super.finish();
     }
 
